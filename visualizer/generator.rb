530b7751c15c83d9318ce3b085ee3433aecc8809
Katrina Verey <kn.verey@gmail.com>
2013-11-03 00:04:49 -0400

    Most notably added method to String to remove_accents and applied that in several places to make alphabetization and output in French meet expectations

--- visualizer/generator.rb
+++ visualizer/generator.rb
@@ -1,7 +1,7 @@
 # require 'erb'
 # require 'sinatra'
-# require 'debugger'
 require 'csv'
+require 'debugger'
 
 require './glossary.rb'
 require './term_record.rb'
@@ -10,12 +10,10 @@ require './term_record.rb'
 #required format: lang1_term, lang2_term
 csv_data = CSV.read("irb_glossary.csv")
 
-#pass in 2nd param true if lang2 is source language
-en_fr_glossary = Glossary.new(csv_data)
-fr_en_glossary = Glossary.new(csv_data, true)
+#pass in 3rd param true if lang2 is source language
+en_fr_glossary = Glossary.new("English to French",csv_data)
+fr_en_glossary = Glossary.new("French to English",csv_data, true)
 
-#TESTS FOR OUTPUT
-# en_fr_glossary.output_records_at("1")
-# en_fr_glossary.output_records_at("#")
-# en_fr_glossary.output_records_at("D")
-fr_en_glossary.output_records_at("D")
+fr_en_glossary.output_records_at("é")
+puts en_fr_glossary.chapter_list
+puts fr_en_glossary.chapter_list
--- visualizer/glossary.rb
+++ visualizer/glossary.rb
@@ -1,20 +1,21 @@
 class Glossary
 	attr_reader :chapter_list
 
-	def initialize(csv_data, reverse=false)
+	def initialize(description, csv_data, reverse=false)
 		@all_records = []
 		@id_to_assign = 0
+		@description = description
 
 		if reverse; source_lang_term = 1; translation = 0
 		else; source_lang_term = 0; translation = 1; end
 
-		csv_data.each do |record|
-			@all_records << TermRecord.new(record[source_lang_term], record[translation])
-		end
-
-		@all_records.sort_by! {|record| record.source_lang_term }
-
+		csv_data.each { |record| @all_records << TermRecord.new(record[source_lang_term], record[translation]) }
+		sort_records
 		assign_ids
+	end
+
+	def sort_records
+		@all_records.sort_by! {|record| record.source_lang_term.remove_accents }
 	end
 
 	def assign_ids
@@ -24,16 +25,32 @@ class Glossary
 		end
 	end
 
+	def chapter_list
+		all_char1s = @all_records.map do |record| 
+			term_char1 = record.source_lang_term[0].remove_accents
+
+			%w[1 2 3 4 5 6 7 8 9 0].include?(term_char1) ? "#" : term_char1.upcase
+		end
+		return all_char1s.uniq.sort.to_s
+	end
+
 	def output_records_at(letter)
 		@all_records.each do |record|
 			term_char1 = record.source_lang_term[0]
 
 			if letter=="#"
 				record.output_record if %w[1 2 3 4 5 6 7 8 9 0].include?(term_char1)
-			elsif term_char1.upcase == letter.upcase
+			elsif term_char1.remove_accents.upcase == letter.remove_accents.upcase
 				record.output_record 
 			end
 		end
+	end
+end
+
+class String
+
+	def remove_accents
+		self.tr('ÀàÂâÄäÉéÈèÊêËëÎîÏïÔôÖöÛûÜüÙùÇç','aaaaaaeeeeeeeeiiiioooouuuuuucc')
 	end
 
 end
