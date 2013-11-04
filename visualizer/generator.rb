e1afc4a5ddfb72be2c2d126766e115133f3fee3f
Katrina Verey <kn.verey@gmail.com>
2013-11-03 20:57:15 -0500

    Generated reverse glossary succesfully. Added fixed positioning for top bar and arrows.

--- visualizer/generator.rb
+++ visualizer/generator.rb
@@ -9,9 +9,10 @@ require './term_record.rb'
 template = File.read("glossary.html.erb")
 csv_data = CSV.read("irb_glossary.csv")
 
-en_fr_glossary = Glossary.new("English to French",csv_data)
-# fr_en_glossary = Glossary.new("French to English",csv_data, true)
+# en_fr_glossary = Glossary.new("English to French",csv_data)
+# html = ERB.new(template, nil, ">").result(en_fr_glossary.get_binding)
+# File.open("en_fr_glossary.html","w") {|file| file.puts html }
 
-html = ERB.new(template, nil, ">").result(en_fr_glossary.get_binding)
-
-File.open("glossary.html","w") {|file| file.puts html }
+fr_en_glossary = Glossary.new("French to English",csv_data, true)
+html = ERB.new(template, nil, ">").result(fr_en_glossary.get_binding)
+File.open("fr_en_glossary.html","w") {|file| file.puts html }
