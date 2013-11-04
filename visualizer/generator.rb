906675b0d35100524ccb055817509bbf1dd3ba86
Katrina Verey <kn.verey@gmail.com>
2013-11-04 00:44:10 -0500

    Loads first page by default. Eliminated arrows and renamed references to carousel.

--- visualizer/generator.rb
+++ visualizer/generator.rb
@@ -9,9 +9,9 @@ require './term_record.rb'
 template = File.read("glossary.html.erb")
 csv_data = CSV.read("irb_glossary.csv")
 
-# en_fr_glossary = Glossary.new("English to French",csv_data)
-# html = ERB.new(template, nil, ">").result(en_fr_glossary.get_binding)
-# File.open("en_fr_glossary.html","w") {|file| file.puts html }
+en_fr_glossary = Glossary.new("English to French",csv_data)
+html = ERB.new(template, nil, ">").result(en_fr_glossary.get_binding)
+File.open("en_fr_glossary.html","w") {|file| file.puts html }
 
 fr_en_glossary = Glossary.new("French to English",csv_data, true)
 html = ERB.new(template, nil, ">").result(fr_en_glossary.get_binding)
